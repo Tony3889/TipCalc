eabb91dbd0eeb48e3182d372d68b3a41d8df45a3
nburns <student@nburns.local>
2016-01-08 10:18:55 -0600

    initial commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,16 +10,121 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var totalBillTextField: UITextField!
+    @IBOutlet weak var myTipLabel: UILabel!
+    @IBOutlet weak var myTotalAmountLabel: UILabel!
+    @IBOutlet weak var tenButton: UIButton!
+    @IBOutlet weak var tipAmountPerPersonLabel: UILabel!
+    
+    @IBOutlet weak var totalAmountPerPerson: UILabel!
+    @IBOutlet weak var fifteenButton: UIButton!
+   
+    @IBOutlet weak var amountOfPeopleLabel: UILabel!
+
+    @IBOutlet weak var twentyButton: UIButton!
+    
+    @IBOutlet weak var twentyFiveButton: UIButton!
+    
+    var people = 1.0
+    
+    
+    
+    @IBAction func upArrow(sender: AnyObject)
+    {
+        ++people
+        amountOfPeopleLabel.text = "\(people)"
+    }
+    
+    @IBAction func downArrow(sender: AnyObject)
+    {
+        if people > 1
+        {
+        --people
+        
+        }
+        amountOfPeopleLabel.text = "\(people)"
+            
+    }
+    
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func tenButtonPressed(sender: AnyObject)
+    {
+        
+        calculateTip(0.1)
+        tenButton.setImage(UIImage(named: "10_unselected_image"), forState: UIControlState.Normal)
+        fifteenButton.setImage(UIImage(named: "15_selected_image"), forState: UIControlState.Normal)
+
+        twentyButton.setImage(UIImage(named: "20_selected_image"), forState: UIControlState.Normal)
+        twentyFiveButton.setImage(UIImage(named: "25_selected_image"), forState: UIControlState.Normal)
+        
+           }
+    
+    
+    
+    @IBAction func fifteenButtonPressed(sender: AnyObject)
+    {
+        calculateTip( 0.15)
+        fifteenButton.setImage(UIImage(named: "15_unselected_image"), forState: UIControlState.Normal)
+        tenButton.setImage(UIImage(named: "10_selected_image"), forState: UIControlState.Normal)
+        twentyButton.setImage(UIImage(named: "20_selected_image"), forState: UIControlState.Normal)
+        twentyFiveButton.setImage(UIImage(named: "25_selected_image"), forState: UIControlState.Normal)
     }
+    func calculateTip(tipPercentage: Double)
+    {
+        var subTotal = ((totalBillTextField.text!) as NSString).doubleValue
+        let tipAmount = (subTotal * tipPercentage) / people
+        let totalAmount = (subTotal + tipAmount) / people
+        
+        myTipLabel.text = String(format: "$ %.2f", tipAmount)
+        myTotalAmountLabel.text = String(format: "$ %.2f", totalAmount)
+        tipAmountPerPersonLabel.text = String(format: "$ %.2f", tipAmount)
+        totalAmountPerPerson.text = String(format: "$ %.2f", totalAmount)
+        totalBillTextField.resignFirstResponder()
+    }
+    
+    
+    
+        
+        
+        
+        
+        @IBAction func twentyButtonPressed(sender: AnyObject)
+    {
+        calculateTip(0.20)
+        twentyButton.setImage(UIImage(named: "20_unselected_image"), forState: UIControlState.Normal)
+        tenButton.setImage(UIImage(named: "10_selected_image"), forState: UIControlState.Normal)
+        fifteenButton.setImage(UIImage(named: "15_selected_image"), forState: UIControlState.Normal)
+        twentyFiveButton.setImage(UIImage(named: "25_selected_image"), forState: UIControlState.Normal)
 
+    
+    
+    }
+    
+   
+        
+        
+        
+        
+        @IBAction func twentyFiveButtonPressed(sender: AnyObject)
+    {
+        calculateTip(0.25)
+        twentyFiveButton.setImage(UIImage(named: "25_unselected_image"), forState: UIControlState.Normal)
+        tenButton.setImage(UIImage(named: "10_selected_image"), forState: UIControlState.Normal)
+        
+        fifteenButton.setImage(UIImage(named: "15_selected_image"), forState: UIControlState.Normal)
+        twentyButton.setImage(UIImage(named: "20_selected_image"), forState: UIControlState.Normal)
 
+    }
+    
+    
+    
 }
 
